37aacb1684adb7afca560e56aa9bd7f7c83feb34
Rodrigo <rodrigorael53@gmail.com>
2022-05-14 04:16:01 -0300

    adding settings for storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack4",
+  },
+};
